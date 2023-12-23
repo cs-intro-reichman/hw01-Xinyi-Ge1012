3ddf215a1f4e7e4614cac3910bcd756c9256c681
Xinyi-Ge1012 <154273174+Xinyi-Ge1012@users.noreply.github.com>
2023-12-23 14:48:59 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -4,6 +4,11 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int Coins  = Integer.parseInt(args[0]);
+		int quarters = Coins/25;
+		int cents = Coins%25;
+
+		System.out.println("Use " + quarters + " quarters and " + cents + " cents");
+	
 	}
 }
