9e0bcbcbb12a5f290ec20b8016bf291eb157d5c4
Xinyi Ge <xinyi.ge@post.runi.ac.il>
2023-12-23 14:06:31 +0200

    test1

--- AddTwo.java
+++ AddTwo.java
@@ -3,8 +3,11 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-	 int a = 3;
-		int b = 5;
+		
+	 int a = Integer.parseInt(args[0]);
+	 int b = Integer.parseInt(args[1]);
+
+
 		
 	System.out.println( a + " + " + b + " = " + ( a + b ) );
 
