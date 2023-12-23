6e4675184291d2baf45773796fb49a06be63cc29
Xinyi-Ge1012 <154273174+Xinyi-Ge1012@users.noreply.github.com>
2023-12-23 20:24:28 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,35 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+	   int MIN = Integer.parseInt(args[0]);
+	   int MAX = Integer.parseInt(args[1]);
+
+	   for (int i = 0; i < 10; i++) {
+        int a = (int) (Math.random() * MAX + MIN);
+		int b = (int) (Math.random() * MAX + MIN);
+		int c = (int) (Math.random() * MAX + MIN);
+		    System.out.print(a);
+		    System.out.print(b);
+		    System.out.print(c);
+
+	   }
+           if (a<b) {
+			 if (a<c){
+			System.out.println( "The minimal generated number was a" );
+			}
+		}
+              else {
+                if (b<c){
+			System.out.println( "The minimal generated number was b" );
+				}
+				  else {
+					System.out.println( "The minimal generated number was c" );
+				  }
+
+			  }
+		   
+	}
+}
+
 	}
 }
